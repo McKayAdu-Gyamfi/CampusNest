5576154ee3fb16236bff04c8b5041ce0f6094e47
SMarco2310 <marc.sossou@ashesi.edu.gh>
2026-05-26 20:02:49 +0000

    fix: update bathroom hotspot yaw value in LivePreview

--- client/src/pages/LivePreview.tsx
+++ client/src/pages/LivePreview.tsx
@@ -24,7 +24,7 @@ export default function LivePreview() {
         hotSpots: [
           {
             pitch: -5,
-            yaw: 185,
+            yaw: 170,
             type: "scene",
             text: "Go to Bathroom",
             sceneId: "bathroom_1"
